56800504434c1e041f8080736c56612c2603fba6
stacief510 <staciemfraser@gmail.com>
2018-08-22 14:29:13 -0700

    updated App.js

--- vinecellars/src/App.js
+++ vinecellars/src/App.js
@@ -1,13 +1,82 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Switch, Route } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
+import Footer from './layout/Footer';
+import Home from './Home';
+import Login from './auth/Login';
+import Nav from './layout/Nav';
+import Signup from './auth/Signup';
 import Header from './components/Header';
+// import Profile from './Profile';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      user: null
+    }
+  }
+
+  componentDidMount = () => {
+    console.log('component did mount!');
+    this.getUser();
+  }
+
+  getUser = () => {
+    console.log('get user');
+    var token = localStorage.getItem('mernToken');
+    if(token){
+      console.log('token found in LS', token);
+      // There is a token in localStorage. Try to validate it!
+      axios.post('/auth/me/from/token', {
+        headers: { 'Authorization': `Bearer ${token}` }
+      })
+      .then(response => {
+        console.log('SUCCESS', response);
+        this.setState({
+          user: response.data.user
+        });
+      })
+      .catch(err => {
+        console.log('ERROR', err);
+        console.log('response', err.response);
+        localStorage.removeItem('mernToken');
+        this.setState({
+          user: null
+        });
+      });
+    }
+    else {
+      console.log('No token was found');
+      localStorage.removeItem('mernToken');
+      this.setState({
+        user: null
+      });
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-      <Header/>
-          <h1 >Welcome to Vine Cellars</h1>
+      <div className="VineCellars">
+        <Header/>
+        <h1>Welcome to Vine Cellars</h1>
+        <Switch>
+          <div className="container">
+            <Nav user={this.state.user} updateUser={this.getUser} />
+            <Route exact path="/users" component={Home} />
+            <Route path="/login" component={
+              () => (<Login user={this.state.user} updateUser={this.getUser} />)
+            } />
+            <Route path="/signup" component={
+              () => (<Signup user={this.state.user} updateUser={this.getUser} />)
+            } />
+            {/* <Route path="/profile" component={
+              () => (<Profile user={this.state.user} />)
+            } /> */}
+          </div>
+        </Switch>
+        <Footer />
       </div>
     );
   }
